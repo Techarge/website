3b52cf32074315089698710523efb07a08125a6a
ZOESCOMPUTER\Zoe <zoeutting@techarge.co.uk>
2021-09-14 12:38:00 +0100

    Start of aws subscribe

--- techarge/assets/js/main.js
+++ techarge/assets/js/main.js
@@ -38,35 +38,3 @@ targets2.forEach(function (target2) {
     target2.classList.add("opacity-0");
     observer2.observe(target2);
 });
-
-const signupButton = document.querySelector("#signup-button");
-if (signupButton) {
-    signupButton.addEventListener('click', function () {
-        //validate
-        const emailValue = document.querySelector('#signup-email').value;
-        var mail_format = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
-        if(!mail_format.test(emailValue)) {
-            document.getElementById("email-status").innerHTML = 'Please enter a valid email address.';
-            document.getElementById("signup-email").css('color', '#8A1111');
-            document.getElementById("signup-email").css('background', '#FFD1D1');
-        }
-        else {
-            document.getElementById("email-status").innerHTML = 'Looks great. We are adding you.';
-            document.getElementById("signup-email").css('color', '#30781C');
-            document.getElementById("signup-email").css('background', '#CFFFC2');
-            var request = new XMLHttpRequest();
-            request.open("GET", "", true);
-            request.onload = function () {
-                if (this.status >= 200 && this.status < 400) {
-                    var result = this.response;
-                    console.log("thank you for signing up!");
-                    window.location.replace("/thanks.html");
-                }
-            }
-            request.onerror = function (xhr, resp, text) {
-                console.log("response:" + resp + " text:" + text);
-            };
-            request.send();
-        }
-    });
-}
