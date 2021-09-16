e4448fc6f5f007321ea0b02d79031bfdff01ae3b
ZOESCOMPUTER\Zoe <zoeutting@techarge.co.uk>
2021-09-16 13:22:43 +0100

    collect email.py

--- techarge/assets/python/collectemail.py
+++ techarge/assets/python/collectemail.py
@@ -1,48 +1,37 @@
-var AWS = require('aws-sdk');
-var ses = newAWS.SES();
-var RECEIVER = 'zoe.utting@techarge.co.uk ';
-var SENDER = 'zoe.utting@techarge.co.uk';
-
-var
-response = {
-    "isBase64Encoded": false,
-    "headers": {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
-    "statusCode": 200,
-    "body": "{\"result\": \"Success.\"}"
-};
-
-exports.handler = function(event, context)
-{
-    console.log('Received event:', event);
-sendEmail(event, function(err, data)
-{
-    context.done(err, null);
-});
-};
-
-function
-sendEmail(event, done)
-{
-    var
-params = {
-    Destination: {
-        ToAddresses: [
-            RECEIVER
-        ]
-    },
-    Message: {
-        Body: {
-            Text: {
-                Data: 'name: ' + event.name + '\nphone: ' + event.phone + '\nemail: ' + event.email + '\ndesc: ' + event.desc,
-                Charset: 'UTF-8'
-            }
-        },
-        Subject: {
-            Data: 'Website Referral Form: ' + event.name,
-            Charset: 'UTF-8'
+import json
+import boto3
+import botocore
+import uuid
+s3 = boto3.client('s3')
+def lambda_handler(event, context):
+    print (boto3.__version__)
+    #check for params
+    if 'queryStringParameters' in event:
+        print (json.dumps(event['queryStringParameters'], indent=4))
+        s3_put_model(
+            str(uuid.uuid4()),
+            'collect/email.techarge.co.uk',
+            event['queryStringParameters'],
+            'techarge.collectemail')
+    body = { "message":"SUCCEED" }
+    response = {
+            'statusCode': 200,
+            'headers': {
+                "Access-Control-Allow-Origin" : "https://website.techarge.co.uk"
+            },
+            'body': json.dumps(body, indent=4)
         }
-    },
-    Source: SENDER
-};
-ses.sendEmail(params, done);
-}
+    return response
+def s3_put_model(fid, prefix, model, bucketName):
+    """
+    send model to bucket
+    """
+    try:
+        bodydata = json.dumps(model, indent=4)
+        bodydata += " test "
+        s3.put_object(Bucket='techarge.collectemail',
+              Key='{0}/{1}.json'.format(prefix,fid),
+              Body=f"{bodydata}")
+    except Exception as e:
+        print(e)
+        raise e
