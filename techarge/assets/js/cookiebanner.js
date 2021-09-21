f06ed1cf0faad9ec4e23f626d30bea4d697402f0
ZOESCOMPUTER\Zoe <zoeutting@techarge.co.uk>
2021-09-21 12:38:24 +0100

    Cookie Banner Remove cookies or allow cookies with further styling

--- techarge/assets/js/cookiebanner.js
+++ techarge/assets/js/cookiebanner.js
@@ -1,11 +1,25 @@
 var cookieContainer = document.querySelector(".cookie-container");
 var cookieButton = document.querySelector(".cookie-btn");
+var disableCookieButton = document.querySelector(".disable-cookies-btn");
 
-if (window.localStorage.getItem('accept_cookies')) {
+if (window.localStorage.getItem('accept_cookies') || window.localStorage.getItem('disable_cookies') ) {
     cookieContainer.classList.remove("active");
 }
+
+disableCookieButton.addEventListener("click", function () {
+    window.localStorage.setItem('disable_cookies', 'true');
+    cookieContainer.classList.remove("active");
+    disableCookies();
+});
 
 cookieButton.addEventListener("click", function () {
     window.localStorage.setItem('accept_cookies', 'true');
     cookieContainer.classList.remove("active");
 });
+
+function disableCookies() {
+    cookieStore.getAll().then(cookies => cookies.forEach(cookie => {
+    console.log('Cookie deleted:', cookie);
+    cookieStore.delete(cookie.name);
+}));
+}
