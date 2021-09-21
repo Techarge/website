ebf0b7d96c26847f083425809e51db09b3a63ff0
ZOESCOMPUTER\Zoe <zoeutting@techarge.co.uk>
2021-09-21 13:45:06 +0100

    Js on firefox

--- techarge/assets/js/cookiebanner.js
+++ techarge/assets/js/cookiebanner.js
@@ -2,7 +2,7 @@ var cookieContainer = document.querySelector(".cookie-container");
 var cookieButton = document.querySelector(".cookie-btn");
 var disableCookieButton = document.querySelector(".disable-cookies-btn");
 
-if (window.localStorage.getItem('accept_cookies') || window.localStorage.getItem('disable_cookies') ) {
+if (window.localStorage.getItem('accept_cookies') || window.localStorage.getItem('disable_cookies')) {
     cookieContainer.classList.remove("active");
 }
 
@@ -17,8 +17,46 @@ cookieButton.addEventListener("click", function () {
     cookieContainer.classList.remove("active");
 });
 
+
+function setCookie(key, value, expireDays, expireHours, expireMinutes, expireSeconds) {
+    var expireDate = new Date();
+    if (expireDays) {
+        expireDate.setDate(expireDate.getDate() + expireDays);
+    }
+    if (expireHours) {
+        expireDate.setHours(expireDate.getHours() + expireHours);
+    }
+    if (expireMinutes) {
+        expireDate.setMinutes(expireDate.getMinutes() + expireMinutes);
+    }
+    if (expireSeconds) {
+        expireDate.setSeconds(expireDate.getSeconds() + expireSeconds);
+    }
+    document.cookie = key + "=" + escape(value) +
+        ";domain=" + window.location.hostname +
+        ";path=/" +
+        ";expires=" + expireDate.toUTCString();
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", null, null, null, 1);
+}
+
 function disableCookies() {
-    cookieStore.getAll().then(cookies => cookies.forEach(cookie => {
-    cookieStore.delete(cookie.name);
-}));
+    /*method depends on browser*/
+    if (navigator.userAgent.includes('Chrome')) {
+                cookieStore.getAll().then(cookies => cookies.forEach(cookie => {
+            cookieStore.delete(cookie.name);
+        }));
+
+    } else {
+deleteCookie("_ga");
+        deleteCookie("ph_cQo4wipp5ipWWXhTN8kTacBItgqo457yDRtzCMOr-Tw_posthog");
+        deleteCookie("_ga_V2CWDTHL7P");
+        deleteCookie("csrftoken");
+    }
+}
+
+function enableCookies() {
+    alert("enabled");
 }
